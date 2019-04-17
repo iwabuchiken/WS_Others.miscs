28f0162ace134ce13b78e67a78a503d361b3085b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2019-04-17 16:08:13 +0900

    myself] res free# JVEMV6 52#7 19 / 52. 自然観察 n.o. / 7. 鳥 birds / 20190417
    steps=
    ----------------
    コオロギ ::[20190417_143446
    
    	==> COMP.

--- JVEMV6/52_nature-observation/7_birds/start_apps.bat
+++ JVEMV6/52_nature-observation/7_birds/start_apps.bat
@@ -29,5 +29,6 @@ pushd "C:\Program Files (x86)\Google\Chrome\Application"
 start chrome.exe "http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=jve+birds&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 start chrome.exe https://www.szynalski.com/tone-generator/
 start chrome.exe "https://docs.google.com/spreadsheets/d/1xkLnhw32NHCxJzZoJHfNfcSK1HwXroyl36teBeF8Xvc/edit#gid=0"
+start chrome.exe "https://docs.google.com/document/d/1MLUozn3rnBjorTs1sOlyAOR_GBwf7BPxz4lYr1e-JfI/edit"
 
 REM exit
