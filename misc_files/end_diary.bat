927e8dfe89a0ac056c366f925f65b6c3c4068e5b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-03-25 14:36:19 +0900

    (WS_Others.miscs : diary, personal) periodical updates

--- misc_files/end_diary.bat
+++ misc_files/end_diary.bat
@@ -1,1 +1,3 @@
-pushd C:\WORKS_2\WS\WS_Others.miscs && git add . && git commit -m "(WS_Others.miscs : diary, personal) periodical updates" && e && git push origin master
+C:\WORKS_2\a.bat && pushd C:\WORKS_2\WS\WS_Others.miscs && git add . && git commit -m "(WS_Others.miscs : diary, personal) periodical updates" && e && git push origin master
+
+pause
